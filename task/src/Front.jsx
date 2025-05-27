df085dd56bcfd921825c656a867bf643803e173e
manishkumar832 <bodakuntimanishkumar@gmail.com>
2025-05-27 21:44:12 +0530

    task update

--- task/src/Front.jsx
+++ task/src/Front.jsx
@@ -2,7 +2,7 @@ import './Front.css'
 function Front() {
     return (
         <div id="front">
-            <img src="./public/architect.jpg" width="98%"/>
+            <img src="	https://www.w3schools.com/w3images/architect.jpg" width="98%"/>
             <p>
                 <span>BR</span>Architect
             </p>
